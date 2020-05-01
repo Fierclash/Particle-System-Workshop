ed32f6c69c1eebd6769a8bb942efad3338f099df
Fierclash <49929727+Fierclash@users.noreply.github.com>
2020-05-01 12:36:36 -0700

    Init Commit

---
+++ Particle Effects Presentation/Assets/Burst.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ *  Creates a burst particle effect on click at the mouse
+ */
+public class Burst : MonoBehaviour
+{
+    public GameObject particles;            // System to spawn
+    public float particleLife;              // How long the particles are in the scene
+
+    private Vector3 mousePosition;          // Mouse Position in World Space
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Detects Mouse Input
+        if(Input.GetMouseButtonDown(0))
+        {
+            // Calculate Mouse Position to World Position
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;       // Shift z position in front of camera
+
+            // Spawn Particle
+            SpawnParticles(mousePosition, Quaternion.identity);
+        }
+    }
+
+    // Spawn Particle Burst Effect
+    // Destroys object after a few seconds
+    void SpawnParticles(Vector3 position, Quaternion rotation)
+    {
+        GameObject lastParticle = Instantiate(particles, position, rotation);
+        Destroy(lastParticle, particleLife);
+    }
+}
---
+++ Particle Effects Presentation/Assets/Final/BurstParticle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurstParticle : MonoBehaviour
+{
+    public GameObject particles;
+    public float particleLife;
+
+    public Vector3 mousePosition;
+
+    void Update()
+    {
+        // Detect Mouse Input
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+            SpawnParticles(mousePosition, Quaternion.identity);
+        }
+    }
+
+    // Spawn a particle burst at a location
+    void SpawnParticles(Vector3 position, Quaternion rotation)
+    {
+        GameObject lastCreatedParticle = Instantiate(particles, position, rotation);
+        Destroy(lastCreatedParticle, particleLife);
+    }
+}
